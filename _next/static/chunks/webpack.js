4df97134f9b017127fcc1682e60858a2256b0c75
Usha Mishra <amdatalabs@gmail.com>
2022-03-04 19:22:43 +0000

    Deploy to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aa3a35d1f4786e0cd02a";
+/******/ 	var hotCurrentHash = "bb342782e5e4cc6025ba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
